978f9440102eee6a622ab18dcb4f3d4943a3a0ad
Alejo <avinetam@gmail.com>
2021-04-04 10:58:25 +0200

    added csvToDB function in Utils

--- src/server.js
+++ src/server.js
@@ -5,10 +5,6 @@ const cors = require("cors");
 require("dotenv").config();
 const config = require("./config/config.js");
 require("./config/mongo");
-const asteroidRouter = require("./resources/Asteroid/asteroid.router");
-const userRouter = require("./resources/User/user.router");
-const authRouter = require("./resources/auth/auth.router");
-const addCSVtoDB = require("./Utils/addCSVtoDB");
 
 const path = require("path");
 const { request } = require("express");
@@ -16,10 +12,6 @@ global.appRoot = path.resolve(__dirname);
 
 const app = express();
 
-const jwtProtection = jwt({
-  secret: process.env.TOKEN_SECRET,
-  algorithms: ["HS256"],
-});
 
 app.use(cors());
 app.use(json());
@@ -30,15 +22,11 @@ app.use(express.json())
 
 app.disable("x-powered-by");
 
-app.use("/api/auth", authRouter);
-app.use("/api/asteroids", jwtProtection, asteroidRouter);
-app.use("/api/users", jwtProtection, userRouter);
 
 const start = async () => {
   try {
     app.listen(config.port, () => {
       console.log(`REST API on http://localhost:${config.port}`);
-      addCSVtoDB();
     });
   } catch (e) {
     console.error(e);
