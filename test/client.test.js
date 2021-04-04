26bf2e623af761211666220b486413c734a3a3e7
Alejo <avinetam@gmail.com>
2021-04-04 15:53:34 +0200

    first test working

--- test/client.test.js
+++ test/client.test.js
@@ -1,11 +1,11 @@
 const request = require("supertest");
 const { setupMongo } = require("./test.setup");
 const { app } = require("../src/server");
-const { expect } = require("@jest/globals");
+const { expect, test } = require("@jest/globals");
 
 setupMongo("ClientTest");
 
-test("Create client should work", async () => {
+test("Create client should work and return 201", async () => {
   const newClient = {
     Name: "test",
     Lastname: "test",
@@ -19,7 +19,7 @@ test("Create client should work", async () => {
   expect(createRes.status).toBe(201);
 });
 
-test("Create the same user more than once should fail", async () => {
+test("Create the same client more than once should fail nad return 400", async () => {
   const newClient = {
     Name: "test",
     Lastname: "test",
@@ -34,4 +34,70 @@ test("Create the same user more than once should fail", async () => {
   expect(create1Res.status).toBe(201);
   expect(create2Res.status).toBe(400);
 
-})
+});
+
+test("Create client without all the relevant data should fail and return 400",
+  async () => {
+    const newClient = {
+      Name: "test",
+      Lastname: "test",
+      Latitude: 180,
+      Longitude: 54,
+    };
+
+    const createRes = await request(app)
+      .post('/api/clients/')
+      .send(newClient);
+
+
+      expect(createRes.status).toBe(400);
+  });
+
+  test("Find all users should work", async ()=> {
+    
+    const newClient = {
+      Name: "test",
+      Lastname: "test",
+      Age: 29,
+      Latitude: 180,
+      Longitude: 54,
+    };
+
+    const createRes = await request(app).post('/api/clients/').send(newClient);
+
+    const findAllRes = await request(app).get('/api/clients/');
+
+    expect(createRes.status).toBe(201);
+    expect(findAllRes.status).toBe(200);
+    expect(findAllRes.body.length).toBeGreaterThan(0);
+
+  });
+
+  test("Get should work", async()=> {
+    
+    const newClient = {
+      Name: "test",
+      Lastname: "test",
+      Age: 29,
+      Latitude: 180,
+      Longitude: 54,
+    };
+
+    const createRes = await request(app).post('/api/clients/').send(newClient);
+
+    const findAllRes = await request(app).get('/api/clients/');
+
+    const _id = findAllRes.body[0]._id;
+
+    const getRes = await request(app).get(`/api/clients/${_id}`);
+
+    expect(createRes.status).toBe(201);
+    expect(findAllRes.status).toBe(200);
+    expect(getRes.status).toBe(200);
+    expect(getRes.body).toBeDefined();
+
+  });
+
+  test("Update should work", async() => {
+    
+  })
