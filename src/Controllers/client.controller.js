acfd6858f53ca32d3e450ab0930bb7b103a69605
Alejo <avinetam@gmail.com>
2021-04-04 12:44:26 +0200

    bugs fixed in client.controller

--- src/Controllers/client.controller.js
+++ src/Controllers/client.controller.js
@@ -1,35 +1,35 @@
-const userModel = require("../Models/client.model");
+const clientModel = require("../Models/client.model");
 const price = require('../Utils/price')
 
 const create = (req, res) => {
     const newClient = req.body;
     // newClient.hotspots_asteroids(newClient.latitude, newClient.longitude)
-    newClient.hotspot_asteroids = 10;
-    newClient.price = price(newClient.age, newClient.hotspot_asteroids);
+    newClient.Hotspot_asteroids = 10;
+    newClient.Price = price(newClient.Age, newClient.Hotspot_asteroids);
     console.log(newClient);
-    userModel.create(newClient);
+    clientModel.create(newClient);
     return res.status(201).json(newClient);
 }
 
 const get = async (req, res) => {
-  const user = await userModel.get(req.params.id);
-  return res.status(200).json(user);
+  const client = await clientModel.get(req.params.id);
+  return res.status(200).json(client);
 };
 
 const update = (req, res) => {
   const updatedUser = req.body;
-  const userUpdated = userModel.update(req.params.id, updatedUser);
-  return res.status(200).json(userUpdated);
+  const clientUpdated = clientModel.update(req.params.id, updatedUser);
+  return res.status(200).json(clientUpdated);
 };
 
 const remove = (req, res) => {
-  const userDeleted = userModel.remove(req.params.id);
-  return res.status(200).json(userDeleted);
+  const clientDeleted = clientModel.remove(req.params.id);
+  return res.status(200).json(clientDeleted);
 };
 
 const findAll = async (req, res) => {
-  const users = await userModel.findAll();
-  return res.status(200).json(users);
+  const clients = await clientModel.findAll();
+  return res.status(200).json(clients);
 };
 
 const addList = (req, res) => {
@@ -39,8 +39,8 @@ const addList = (req, res) => {
         newClient.hotspot_asteroids = 10;
         newClient.price = price(newClient.age, newClient.hotspot_asteroids);
   });
-  const usersCreated = userModel.addList(newClients);
-  return res.status(200).json(usersCreated);
+  const clientsCreated = clientModel.addList(newClients);
+  return res.status(200).json(clientsCreated);
 };
 
 module.exports = {
